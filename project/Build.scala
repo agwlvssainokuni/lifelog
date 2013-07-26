9c3f43404f5f24fc42e41e6a6bd25e8623979a93
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2013-07-26 22:25:55 +0900

    lifelog * サブプロジェクト構成を定義。

---
+++ project/Build.scala
@@ -0,0 +1,53 @@
+import sbt._
+import Keys._
+import play.Project._
+
+object ApplicationBuild extends Build {
+
+  val appName         = "lifelog"
+  val appVersion      = "0.0"
+
+  val commonDeps = Seq(
+    jdbc,
+    anorm
+  )
+
+  val adminDeps = Seq(
+  )
+
+  val websiteDeps = Seq(
+  )
+
+  val commonProj = play.Project(
+    appName + "-common",
+    appVersion,
+    commonDeps,
+    path = file(appName + "-common")
+  )
+
+  val adminProj = play.Project(
+    appName + "-admin",
+    appVersion,
+    adminDeps,
+    path = file(appName + "-admin")
+  ).dependsOn(commonProj)
+
+  val websiteProj = play.Project(
+    appName + "-website",
+    appVersion,
+    websiteDeps,
+    path = file(appName + "-website")
+  ).dependsOn(commonProj)
+
+  val main = play.Project(
+    appName,
+    appVersion
+  ).dependsOn(
+    adminProj,
+    websiteProj
+  ).aggregate(
+    adminProj,
+    websiteProj
+  )
+
+}
