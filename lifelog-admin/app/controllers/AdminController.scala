b148ea59af31171dfddb18146db9e5fd23bbfc55
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2013-07-28 09:03:32 +0900

    lifelog - admin * 管理者アカウント機能。

---
+++ lifelog-admin/app/controllers/AdminController.scala
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2013 agwlvssainokuni
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import play.api.mvc.Controller
+
+object AdminController extends Controller {
+
+  def list(pn: Long = 0, ps: Long = 5) = TODO
+
+  def add() = TODO
+
+  def create() = TODO
+
+  def edit(id: Long) = TODO
+
+  def update(id: Long) = TODO
+
+  def editPw(id: Long) = TODO
+
+  def updatePw(id: Long) = TODO
+
+  def delete(id: Long) = TODO
+
+}
