9cbce179e1487f0ab238b7eafd50b658e508a723
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2013-08-13 23:54:38 +0900

    lifelog - website * ダイエット記録のデータ管理

--- lifelog-website/app/controllers/DataMgmtController.scala
+++ lifelog-website/app/controllers/DataMgmtController.scala
@@ -16,7 +16,11 @@
 
 package controllers
 
+import scala.concurrent.ExecutionContext.Implicits.global
+
 import PageParam.implicitPageParam
+import models._
+import play.api.libs.iteratee._
 import play.api.mvc._
 
 object DataMgmtController extends Controller with ActionBuilder {
@@ -28,12 +32,43 @@ object DataMgmtController extends Controller with ActionBuilder {
 
   def dietlogExport() = AuthnCustomAction { memberId =>
     implicit conn => implicit req =>
-      NotImplemented
+      val content = (for {
+        (row, i) <- DietLog.stream(memberId).zipWithIndex
+        record = (row.asList.map {
+          case Some(v) => escape(v.toString)
+          case None => ""
+          case v => escape(v.toString)
+        }).mkString(",")
+      } yield {
+        if (i <= 0) {
+          val h = row.metaData.ms.map(m => escape(m.column.alias.getOrElse(""))).mkString(",")
+          Seq(h + newline, record + newline)
+        } else {
+          Seq(record + newline)
+        }
+      }).flatten
+      sendFile("dietlog", content)
   }
 
   def dietlogImport() = AuthnCustomAction { memberId =>
     implicit conn => implicit req =>
       NotImplemented
   }
+
+  private def sendFile[T](basename: String, content: Stream[String]) =
+    Ok.stream(Enumerator.enumerate(content)).withHeaders(
+      CONTENT_TYPE -> play.api.libs.MimeTypes.forExtension("csv").getOrElse(play.api.http.ContentTypes.BINARY),
+      CONTENT_DISPOSITION -> ("""attachment; filename="%s"""".format(filename(basename))))
+
+  private def filename(basename: String) = {
+    val sdf = new java.text.SimpleDateFormat("yyyyMMddHHmmss")
+    val dtm = sdf.format(java.util.Calendar.getInstance.getTime)
+    "%s_%s.csv".format(basename, dtm)
+  }
+
+  private def newline = "\r\n"
+
+  private def escape(s: String) =
+    "\"" + s.flatMap(c => if (c == '"') "\"\"" else c.toString) + "\""
 
 }
